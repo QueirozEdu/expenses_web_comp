43465364f8a2f8776470bcaf4665609c7ab91f3a
Eduardo <eduhmq@gmail.com>
2022-09-01 13:07:42 -0300

    js to show data on bar label

--- services/index.js
+++ services/index.js
@@ -6,7 +6,15 @@ async function getData() {
 
 async function showData() {
     let newData = await getData();
-    console.log(newData["mon"]);
+
+    document.getElementById("mon__amount").innerText = "$".concat(newData["mon"]);
+    document.getElementById("tue__amount").innerText = "$".concat(newData["tue"]);
+    document.getElementById("wed__amount").innerText = "$".concat(newData["wed"]);
+    document.getElementById("thu__amount").innerText = "$".concat(newData["thu"]);
+    document.getElementById("fri__amount").innerText = "$".concat(newData["fri"]);
+    document.getElementById("sat__amount").innerText = "$".concat(newData["sat"]);
+    document.getElementById("sun__amount").innerText = "$".concat(newData["sun"]);
+
 }
 
 showData();
